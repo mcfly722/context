19adf0f8b32a848c82ea1970a4469cec73ac5d34
mcfly722 <7221798@gmail.com>
2023-10-04 00:20:35 +0300

    contextedInstance description

--- contextedInstance.go
+++ contextedInstance.go
@@ -1,6 +1,24 @@
 package context
 
-// ContextedInstance ...
+// This interface should implement by your nodes.
+//
+// Module automatically starts Go(...) method with current Context and automatically waits till it ends.
+// You could not exit from this method without context cancelling (otherwise [ExitFromContextWithoutCancelPanic] occurs).
+//
+// Example:
+//
+//	type node struct {}
+//	func (node *node) Go(current context.Context) {
+//		loop:
+//		for {
+//			select {
+//			case _, isOpened := <-current.Context():
+//				if !isOpened {
+//					break loop
+//				}
+//			}
+//		}
+//	}
 type ContextedInstance interface {
 	Go(current Context)
 }
